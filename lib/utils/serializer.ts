0e3093e7aa42e91eb2348b1b3afe904ca34c8b75
Maxwell Macedo <maxwellmacedo2015@gmail.com>
2025-04-02 02:40:01 -0300

    fix: not mapping ids

--- lib/utils/serializer.ts
+++ lib/utils/serializer.ts
@@ -1,6 +1,10 @@
+import { ID } from "../domain";
+
 export function serializeProps(props: Record<any, any>): Record<any, any> {
   return Object.entries(props).reduce((acc, [key, value]) => {
-    if (typeof value?.toObject === "function") {
+    if (value instanceof ID) {
+      acc[key] = value.value();
+    } else if (typeof value?.toObject === "function") {
       acc[key] = value.toObject();
     } else {
       acc[key] = value;
