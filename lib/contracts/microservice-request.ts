7fd67b19104a3825f359247dd4bdf2bdee763a46
Maxwell Macedo <maxwellmacedo2015@gmail.com>
2025-04-05 18:21:34 -0300

    feat: add request metadata

--- lib/contracts/microservice-request.ts
+++ lib/contracts/microservice-request.ts
@@ -1,6 +1,11 @@
+export interface Metadata {
+  authorization?: string;
+  requestId?: string;
+  correlationId?: string;
+  [key: string]: string | undefined;
+}
+
 export interface MicroserviceRequest<T = any> {
-  headers: Record<string, string>;
-  body: T;
-  params: Record<string, string>;
-  query: Record<string, string>;
+  metadata: Metadata;
+  data: T;
 }
